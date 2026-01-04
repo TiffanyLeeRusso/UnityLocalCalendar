4a7971300345ad23165e632186daa146264fc7f0
TiffanyLeeRusso <tiffanyleerusso@gmail.com>
2026-01-04 18:27:57 +0900

    Add files via upload

--- Assets/Scripts/App/AppBootstrap.cs
+++ Assets/Scripts/App/AppBootstrap.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using LocalCalendar.Data;
+using LocalCalendar.Notifications;
+using Unity.Notifications.Android;
+using System;
 
 namespace LocalCalendar.App
 {
@@ -8,7 +11,31 @@ namespace LocalCalendar.App
         void Awake()
         {
             Database.Initialize();
+            NotificationInitializer.Initialize();
             DontDestroyOnLoad(gameObject);
+        }
+
+        void Start()
+        {
+            var repo = new CalendarRepository();
+            var all = repo.GetAll();
+
+            foreach (var item in all)
+            {
+                NotificationScheduler.Schedule(item);
+            }
+
+            NotificationCatchUpService.Run();
+
+            AndroidNotificationCenter.OnNotificationReceived += data =>
+            {
+                NotificationLogger.Log(new NotificationLogEntry
+                {
+                    Title = data.Notification.Title,
+                    FiredLocal = DateTime.Now,
+                    Note = "Delivered"
+                });
+            };
         }
     }
 }
