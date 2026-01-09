dec6f89ed4231ffe38d3138850866ba591937b71
TiffanyLeeRusso <tiffanyleerusso@gmail.com>
2026-01-09 15:41:48 +0900

    Add files via upload

--- Assets/Scripts/App/AppBootstrap.cs
+++ Assets/Scripts/App/AppBootstrap.cs
@@ -1,31 +1,117 @@
 using UnityEngine;
+using Unity.Notifications.Android;
+using UnityEngine.SceneManagement;
+using System;
 using LocalCalendar.Data;
 using LocalCalendar.Notifications;
-using Unity.Notifications.Android;
-using System;
+using LocalCalendar.Services;
 
 namespace LocalCalendar.App
 {
     public class AppBootstrap : MonoBehaviour
     {
+        private static bool _initialized = false;
+
         void Awake()
         {
+            if (_initialized)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _initialized = true;
+            DontDestroyOnLoad(gameObject);
+
             Database.Initialize();
             NotificationInitializer.Initialize();
-            DontDestroyOnLoad(gameObject);
+
+            LoggingService.Info(LogCategory.System, "App opened");
         }
 
         void Start()
         {
+            // Schedule notifications once per app launch
+            RescheduleNotifications();
+            NotificationCatchUpService.Run();
+
+            // Register listener
+            AndroidNotificationCenter.OnNotificationReceived += OnNotificationReceived;
+
+            // If the app opened from a notification tap
+            HandleNotificationIntent();
+        }
+
+        void HandleNotificationIntent()
+        {            
+            var intent = AndroidNotificationCenter.GetLastNotificationIntent();
+            // If Android gave us an intent
+            if (intent != null && !string.IsNullOrEmpty(intent.Notification.IntentData))
+            {
+                // If this intent is to open an item
+                if (intent.Notification.IntentData.StartsWith("open:item:"))
+                {
+                    // Open the item
+                    var itemId = intent.Notification.IntentData.Replace("open:item:", "");
+                    EditItemContext.EditingItemId = itemId;
+                    SceneManager.LoadScene("EditItemScene");
+                }
+            }
+        }
+
+        private void RescheduleNotifications()
+        {
             var repo = new CalendarRepository();
-            var all = repo.GetAll();
+            var all = repo.GetAllCalendarItems();
+            var nowUtc = DateTime.UtcNow;
 
             foreach (var item in all)
             {
+                if(NotificationScheduler.isCurrentReminder(item))
+                {
+                    NotificationScheduler.Cancel(item);
+                    NotificationScheduler.Schedule(item);
+                }
+            }
+        }
+
+        private void OnNotificationReceived(AndroidNotificationIntentData data)
+        {
+            NotificationLogger.Log(new NotificationLogEntry
+            {
+                Title = data.Notification.Title,
+                FiredLocal = DateTime.Now,
+                Note = "Delivered"
+            });
+        }
+
+        void OnApplicationPause(bool paused)
+        {
+            LoggingService.Info(LogCategory.App,
+                                paused ? "App paused" : "App resumed");
+        }
+
+        private void OnDestroy()
+        {
+            // Defensive cleanup (good practice)
+            AndroidNotificationCenter.OnNotificationReceived -= OnNotificationReceived;
+        }
+
+
+        /*
+        void Start()
+        {
+            // Notifications
+            var repo = new CalendarRepository();
+            var all = repo.GetAllCalendarItems();
+
+            foreach (var item in all)
+            {
+                NotificationScheduler.Cancel(item);
                 NotificationScheduler.Schedule(item);
             }
 
             NotificationCatchUpService.Run();
+            NotificationRescheduler.RescheduleAll();
 
             AndroidNotificationCenter.OnNotificationReceived += data =>
             {
@@ -37,5 +123,7 @@ namespace LocalCalendar.App
                 });
             };
         }
+        */
+        
     }
 }
