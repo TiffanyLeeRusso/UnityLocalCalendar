06007e997f0e574787fbe34bf9feda383ccb62df
TiffanyLeeRusso <tiffanyleerusso@gmail.com>
2026-01-28 18:15:17 +0900

    Delete Assets/Scripts/Notifications directory

--- Assets/Scripts/Notifications/NotificationSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace LocalCalendar.Notifications
-{
-    public static class NotificationSettings
-    {
-        // Adjust later if needed
-        public const int EarlyFireBufferMinutes = 2;
-    }
-}
